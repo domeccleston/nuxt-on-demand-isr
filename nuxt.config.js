d7854fc1fbb18c9472f93a6e62c43c3b80c4a409
Dom <domeccleston@gmail.com>
2023-11-01 16:53:48 +0000

    target edge

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  nitro: {
+    preset: 'vercel-edge',
+  },
+
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
